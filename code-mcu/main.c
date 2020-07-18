45aa4e0ab0f1913d6069b7e4e05926c3e26e257d
Nator Verinumbe <natverior1@gmail.com>
2020-07-18 19:36:49 +0100

    initialized Atmel project file in code-mcu

---
+++ code-mcu/main.c
@@ -0,0 +1,18 @@
+/*
+ * code-mcu.c
+ *
+ * Created: 7/18/2020 7:35:10 PM
+ * Author : Nator Verinumbe
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
